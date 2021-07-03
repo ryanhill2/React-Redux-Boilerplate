46f13edc78d7011dfdbe0949d398d0f1cbffd078
ryan <ryan.hill1@mydit.ie>
2021-07-04 00:46:09 +0100

    refactoring actions into separate actions folder

---
+++ React-Redux-Typescript/src/state/reducers/bankReducers.ts
@@ -0,0 +1,16 @@
+import {Action} from "../../actions";
+
+const initialState = 0;
+
+const reducer = (state: number = initialState, action: Action) => {
+    switch (action.type){
+        case "deposit":
+            return state + action.payload;
+        case "withdraw":
+            return state - action.payload;
+        case "bankrupt":
+            return 0;
+    }
+}
+
+export default reducer
